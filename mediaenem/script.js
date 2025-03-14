9dbb381fb7167b44c132c1bedec9419e6ab26ed7
Gabriel Letto <158781940+Truperymanx@users.noreply.github.com>
2025-03-14 17:09:13 -0300

    alteraçoes em projetos e criaçao de novo projeto

---
+++ mediaenem/script.js
@@ -0,0 +1,7 @@
+function calcular(){
+    let mat = document.getElementById('mat')
+    let hum = document.getElementById('huma')
+    let ling = document.getElementById('ling')
+    let nat = document.getElementById('natu')
+    let reda = document.getElementById('reda')
+}
